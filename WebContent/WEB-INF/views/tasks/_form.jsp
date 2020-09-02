489ac785e235df72366e6d0bc9883a4591db8bc4
さとゆり <ykys91327@gmail.com>
2020-09-02 18:27:06 +0900

    Modify Index/NewServlet

---
+++ WebContent/WEB-INF/views/tasks/_form.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<label for="title">タイトル</label><br />
+<input type="text" name="title" value="${task.title}" />
+<br /><br />
+
+<label for="content">タスク</label><br />
+<input type="text" name="content" value="${task.content}" />
+<br /><br />
+
+<input type="hidden" name="_token" value="${_token}" />
+<button type="submit">投稿</button>
--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,7 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
 
-import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,19 +10,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import utils.DBUtil;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class NewServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public NewServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -33,17 +30,14 @@ public class IndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
 
-        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class)
-                              .getResultList();
+        request.setAttribute("_token", request.getSession().getId());
 
-        em.close();
+        request.setAttribute("task", new Task());
 
-        request.setAttribute("tasks", tasks);
-
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
         rd.forward(request, response);
+
 
     }
 
