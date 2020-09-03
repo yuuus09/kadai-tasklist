1c9e417e0183735a256e06b25936f60496ad938e
さとゆり <ykys91327@gmail.com>
2020-09-04 05:12:16 +0900

    Add pagination

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -35,10 +35,26 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
+        int page = 1;
+
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) {}
+
+
         List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class)
+                              .setFirstResult(15 * (page - 1))
+                              .setMaxResults(15)
                               .getResultList();
 
+        long tasks_count = (long)em.createNamedQuery("getTasksCount", Long.class)
+                .getSingleResult();
+
         em.close();
+
+        request.setAttribute("tasks", tasks);
+        request.setAttribute("tasks_count", tasks_count);
+        request.setAttribute("page", page);
 
         request.setAttribute("tasks", tasks);
 
--- src/models/Task.java
+++ src/models/Task.java
@@ -16,6 +16,10 @@ import javax.persistence.Table;
     @NamedQuery(
             name = "getAllTasks",
             query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
+            ),
+    @NamedQuery(
+            name = "getTasksCount",
+            query = "SELECT COUNT(t) FROM Task AS t"
             )
 })
 
